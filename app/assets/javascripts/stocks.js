c8fcee28d843d2112f85c7c7f5571aa15f41ac63
Greg Mahring <greg.mahring@leandogsoftware.com>
2017-04-11 21:54:31 -0400

    Stock lookup working again

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,14 +1,8 @@
 var init_stock_lookup;
-
 init_stock_lookup = function() {
-    $('#stock-lookup-form').on('ajax-success', function(event, data, status){
+    $('#stock-lookup-form').on('ajax:success', function(event, data, status) {
         $('#stock-lookup').replaceWith(data);
         init_stock_lookup();
-    });
-
-    $('#stock-lookup-form').on('ajax-error', function(event, xhr, status, error){
-        $('#stock-lookup-results').replaceWith('');
-        $('#stock-lookup-errors').replaceWith('Stock was not found.');
     });
 }
 
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,8 @@
 class StocksController < ApplicationController
-
   def search
     if params[:stock]
       @stock = Stock.find_by_ticker(params[:stock])
+
       @stock ||= Stock.new_from_lookup(params[:stock])
     end
 
